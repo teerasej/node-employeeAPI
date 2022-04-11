429b24ab1e57fde1cbdbe53b6cd364423e483f3f
Teerasej Jiraphatchandej <teerasej@gmail.com>
2022-04-11 20:58:09 +0700

    switch to use employee schema

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 
 // เรียกใช้งาน express module 
 const express = require('express');
-const { enable } = require('express/lib/application');
 const Joi = require('joi');
 
 let app = express();
@@ -11,9 +10,12 @@ const PORT = process.env.PORT
 
 const MONGO_HOST = process.env.MONGO_DB_HOST
 const MONGO_PORT = process.env.MONGO_DB_PORT
-console.log('mongo env:',MONGO_HOST, MONGO_PORT)
+console.log('mongo env:', MONGO_HOST, MONGO_PORT)
 
-const db = require('monk')(`${MONGO_HOST}:${MONGO_PORT}/employeeDB`)
+// const db = require('monk')(`${MONGO_HOST}:${MONGO_PORT}/employeeDB`)
+const mongoose = require('mongoose');
+const EmployeeModel = require('./model/employee');
+mongoose.connect(`mongodb://${MONGO_HOST}:${MONGO_PORT}/employeeDB`)
 
 app.use(express.json());
 
@@ -24,43 +26,51 @@ app.get('/', (request, respond) => {
 
 app.get('/employees', async (req, res) => {
 
-    const employeeCollection = db.get('employees')
-    const collection = await employeeCollection.find({})
+    const docs = await EmployeeModel.find()
+    res.status(200).json(docs)
 
-    res.json(collection);
 })
 
 app.post('/employees', async (req, res) => {
-    
+
     const newEmployee = req.body;
 
-    const objectSchema = Joi.object({
-        firstName: Joi.string().min(3).required().messages({
-            "string.min": "Must more than 3 characters",
-            "any.required":"Email is required!"
-        }),
-        lastName: Joi.string().min(3).required(),
-        email: Joi.string().email().required().messages({
-            "any.required":"Email is required!"
-        })
-    })
-    const { error } = objectSchema.validate(newEmployee)
-
-
-    if(error) {
-        console.log(error)
-        res.status(400).json({ message: error.details[0].message })
+    try {
+        const newEmployeeModel = new EmployeeModel(newEmployee)
+        const createdEmployee = await newEmployeeModel.save()
+        res.json(createdEmployee)
+    } catch (error) {
+        res.status(401).send(error.message)
     }
 
-    console.log('New employee:', newEmployee);
-    
-    const employeeCollection = db.get('employees')
-    const insertedDocument = await employeeCollection.insert(newEmployee)
 
-    res.json({ message: 'ok', doc: insertedDocument })
+    // const objectSchema = Joi.object({
+    //     firstName: Joi.string().min(3).required().messages({
+    //         "string.min": "Must more than 3 characters",
+    //         "any.required":"Email is required!"
+    //     }),
+    //     lastName: Joi.string().min(3).required(),
+    //     email: Joi.string().email().required().messages({
+    //         "any.required":"Email is required!"
+    //     })
+    // })
+    // const { error } = objectSchema.validate(newEmployee)
+
+
+    // if(error) {
+    //     console.log(error)
+    //     res.status(400).json({ message: error.details[0].message })
+    // }
+
+    // console.log('New employee:', newEmployee);
+
+    // const employeeCollection = db.get('employees')
+    // const insertedDocument = await employeeCollection.insert(newEmployee)
+
+    // res.json({ message: 'ok', doc: insertedDocument })
 })
 
 // กำหนด port และเริ่มการทำงาน
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`server is running at port http://localhost:${PORT}`);
 })
